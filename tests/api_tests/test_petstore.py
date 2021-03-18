6e9c998919c0381a3a5d6f6fceca4f106ddcc6f3
jjuulliiaagg <Ty4kayes@yandex.ru>
2021-03-18 20:07:20 +0300

    fix comments

--- tests/api_tests/test_petstore.py
+++ tests/api_tests/test_petstore.py
@@ -1,11 +1,18 @@
+import allure
 from framework.api.petstore_api_steps import PetstoreUser
 
 
+@allure.story("User creation, login, get user info, logout and user delete")
 def test_petstore_api(api_setup):
     user_data = api_setup
     p_user = PetstoreUser()
-    p_user.create_user(user_data)
-    p_user.user_login(user_data['username'], user_data['password'])
-    p_user.get_correct_user_data(user_data)
-    p_user.user_logout()
-    p_user.delete_user(user_data['username'])
+    with allure.step("Create User"):
+        p_user.create_user(user_data)
+    with allure.step("Login under created user"):
+        p_user.user_login(user_data['username'], user_data['password'])
+    with allure.step("Get user info"):
+        p_user.get_correct_user_data(user_data)
+    with allure.step("User logout"):
+        p_user.user_logout()
+    with allure.step("User delete"):
+        p_user.delete_user(user_data['username'])
